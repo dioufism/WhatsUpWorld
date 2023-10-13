7015f44200f772471c11f742a82e5aa3c2ee3a78
Ousmane <dioufism@gmail.com>
2023-10-12 19:25:58 -0500

    Initial Commit

---
+++ WhatsUpWorld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsUpWorld
+//
+//  Created by ousmane diouf on 10/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsUpWorld/WhatsUpWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpWorldApp.swift
+//  WhatsUpWorld
+//
+//  Created by ousmane diouf on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
